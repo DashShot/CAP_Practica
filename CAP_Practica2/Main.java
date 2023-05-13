07cec90a4cdb92bc7b5d0a8f758bb108ee689753
DavidRG25 <davidrodriguezg25@gmail.com>
2023-05-13 20:10:17 +0200

    Inicio de la practica2

---
+++ CAP_Practica2/Main.java
@@ -0,0 +1,13 @@
+package gic.cap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        example();
+    }
+
+    private static void example() {
+        Example example = new Example();
+        example.run();
+    }
+}
